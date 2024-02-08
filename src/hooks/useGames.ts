a3c0949206108a78b506e1e6d848751254cec26c
Melting-Sun <elotfi51@gmail.com>
2024-02-09 00:51:41 +0330

    displaying critic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
-export interface Platform{
+export interface Platform {
   id: string;
   name: string;
   slug: string;
@@ -12,7 +12,8 @@ export interface Game {
   id: number;
   name: string;
   background_image: string;
-  parent_platforms: { platform: Platform }[]
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
 interface FetchGameResponse {
