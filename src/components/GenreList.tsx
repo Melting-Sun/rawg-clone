47484a058c7d2728a911af42a9c098071a80f9e6
erfan <98186931+Melting-Sun@users.noreply.github.com>
2024-02-14 10:49:48 +0330

    spinner for genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,20 +1,24 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 export default function GenreList() {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+
+  if (error) return null;
+  if (isLoading) return <Spinner />;
+
   return (
     <List>
       {data.map((genre) => (
-        <ListItem key={genre.id} paddingY='5px'>
+        <ListItem key={genre.id} paddingY="5px">
           <HStack>
             <Image
               boxSize="32px"
               borderRadius={8}
               src={getCroppedImageUrl(genre.image_background)}
             />
-            <Text fontSize='lg'>{genre.name}</Text>
+            <Text fontSize="lg">{genre.name}</Text>
           </HStack>
         </ListItem>
       ))}
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -10,7 +10,7 @@ interface FetchResponse<T> {
 const useData = <T>(endpoint: string) => {
     const [data, setData] = useState<T[]>([]);
     const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
   
     useEffect(() => {
       const controller = new AbortController();
