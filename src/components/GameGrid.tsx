7ef0d9c36cf186bfe9526efa39d50eb007418c96
erfan <98186931+Melting-Sun@users.noreply.github.com>
2024-02-19 17:57:29 +0330

    delete unwanted import

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid, Skeleton, Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
