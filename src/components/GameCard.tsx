4ea9fad3914e12a86d95373d4240943763968d77
erfan <98186931+Melting-Sun@users.noreply.github.com>
2024-02-19 09:01:37 +0330

    clean up the game card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game;
@@ -12,10 +13,13 @@ export default function GameCard({ game }: Props) {
       <Card>
         <Image src={game.background_image} />
         <CardBody>
-          <Heading fontSize="2xl">{game.name}</Heading>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
+          <HStack justifyContent={"space-between"} marginBottom={3}>
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+          <Heading fontSize="xl">{game.name}</Heading>
         </CardBody>
       </Card>
     </>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -16,8 +16,8 @@ export default function GameGrid({ gameQuery }: Props) {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={3}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        spacing={6}
         padding={10}
       >
         {isLoading &&
